70113691d439ed71ee1203232ba7f54a94c52ded
David Maus <dmaus@dmaus.name>
2020-02-10 15:29:42 +0100

    Implement XSL transformation via commandline

---
+++ src/main/java/name/dmaus/schxslt/testsuite/impl/CommandlineBuilder.java
@@ -0,0 +1,80 @@
+/*
+ * Copyright (C) 2020 by David Maus <dmaus@dmaus.name>
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use, copy,
+ * modify, merge, publish, distribute, sublicense, and/or sell copies
+ * of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+ * BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+ * ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package name.dmaus.schxslt.testsuite.impl;
+
+import java.nio.file.Path;
+
+import java.util.Map;
+import java.util.ArrayList;
+
+class CommandlineBuilder
+{
+    Path target;
+    Path document;
+    Path stylesheet;
+    Map<String, String> parameters;
+
+    CommandlineBuilder setDocument (final Path document)
+    {
+        this.document = document;
+        return this;
+    }
+
+    CommandlineBuilder setStylesheet (final Path stylesheet)
+    {
+        this.stylesheet = stylesheet;
+        return this;
+    }
+
+    CommandlineBuilder setTarget (final Path target)
+    {
+        this.target = target;
+        return this;
+    }
+
+    CommandlineBuilder setParameters (final Map<String, String> parameters)
+    {
+        this.parameters = parameters;
+        return this;
+    }
+
+    String [] build ()
+    {
+        ArrayList<String> commands = new ArrayList<String>();
+        commands.add("xsltproc");
+        if (parameters != null) {
+            for (Map.Entry<String, String> entry : parameters.entrySet()) {
+                commands.add("-stringparam");
+                commands.add(entry.getKey());
+                commands.add(entry.getValue());
+            }
+        }
+        commands.add("-o");
+        commands.add(target.toAbsolutePath().toString());
+        commands.add(stylesheet.toAbsolutePath().toString());
+        commands.add(document.toAbsolutePath().toString());
+        return commands.toArray(new String[0]);
+    }
+}
---
+++ src/main/java/name/dmaus/schxslt/testsuite/impl/CommandlineTransformer.java
@@ -0,0 +1,76 @@
+/*
+ * Copyright (C) 2020 by David Maus <dmaus@dmaus.name>
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use, copy,
+ * modify, merge, publish, distribute, sublicense, and/or sell copies
+ * of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+ * BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+ * ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package name.dmaus.schxslt.testsuite.impl;
+
+import java.nio.file.Path;
+import java.nio.file.Files;
+
+import java.io.IOException;
+
+import java.util.Map;
+import java.util.HashMap;
+
+/*
+ * Perform an XSL transformation by executing a commandline application.
+ */
+class CommandlineTransformer
+{
+    final Map<String, String> parameters = new HashMap<String, String>();
+    final Path stylesheet;
+
+    CommandlineTransformer (final Path stylesheet)
+    {
+        this.stylesheet = stylesheet;
+    }
+
+    void setParameter (final String name, final String value)
+    {
+        parameters.put(name, value);
+    }
+
+    Path transform (final Path document) throws IOException, InterruptedException
+    {
+        Path target = Files.createTempFile(null, null);
+
+        CommandlineBuilder commandlineBuilder = new CommandlineBuilder();
+        commandlineBuilder
+            .setDocument(document)
+            .setStylesheet(stylesheet)
+            .setParameters(parameters)
+            .setTarget(target);
+
+        String [] call = commandlineBuilder.build();
+
+        Runtime runtime = Runtime.getRuntime();
+        Process process = runtime.exec(call);
+
+        int exitcode = process.waitFor();
+        if (exitcode != 0) {
+            throw new RuntimeException("Executing the external command failed with exit code " + exitcode);
+        }
+
+        return target;
+    }
+}
