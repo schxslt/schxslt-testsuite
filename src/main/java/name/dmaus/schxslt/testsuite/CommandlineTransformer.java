f100b5438466461fdccb2c21297483b9417a4fec
David Maus <dmaus@dmaus.name>
2020-04-09 11:36:24 +0200

    Use shutdown hook to delete temporary file

--- src/main/java/name/dmaus/schxslt/testsuite/CommandlineTransformer.java
+++ src/main/java/name/dmaus/schxslt/testsuite/CommandlineTransformer.java
@@ -55,7 +55,7 @@ class CommandlineTransformer
     Path transform (final Path document) throws IOException, InterruptedException
     {
         Path target = Files.createTempFile(null, null);
-        target.toFile().deleteOnExit();
+        DeleteTemporaryFiles.add(target);
 
         commandlineBuilder
             .reset()
